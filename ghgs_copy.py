8d4a5122b8e7d33b6eff9e8df8aceec224b0c59f
Tom Adams <tom@holizz.com>
2009-08-31 00:24:28 +0100

    Works? Who knows - I keep getting blocked.

--- ghgs_copy.py
+++ ghgs_copy.py
@@ -7,19 +7,26 @@
 
 # Shorthand: gh=GitHub, gs=Gitorious
 
+import getpass
 import json
 import lxml.html
 import os
 import re
+import shutil
+import subprocess
 import tempfile
+import urllib
 import urllib2
 
 class Copier:
 
     gh_repos = 'http://github.com/api/v2/json/repos/show/%(user)s'
     gh_clone = 'git://github.com/%(user)s/%(repo)s'
-    gs_project = 'http://gitorious.org/%(proj)s'
-    gs_regexp = '^git://gitorious.org/%(proj)s/(.+)\.git$'
+    gs_login = 'https://secure.gitorious.org/sessions'
+    gs_project = 'http://gitorious.org/%(project)s'
+    gs_create = 'http://gitorious.org/%(project)s/repositories'
+    gs_regexp = '^git://gitorious.org/%(project)s/(.+)\.git$'
+    gs_push = 'git@gitorious.org:%(project)s/%(repo)s.git'
 
     def __init__(self):
         self.conf = json.load(open('conf.json'))
@@ -36,50 +43,74 @@ class Copier:
                 if not e.errno == 17: # dir already exists
                     raise
 
-        # Clone GitHub repos
         gh_repos = self.gh_list_repos()
-        print gh_repos
-        #TODO
-
-        # List repos in Gitorious
         gs_repos = self.gs_list_repos()
-        print gs_repos
 
-        # Create repos if not existing
-        #TODO
+        for repo in gh_repos:
+            # Clone GitHub repo
+            d = self.conf['github']
+            d.update({'repo': repo})
+            rurl = self.gh_clone % d
+            rpath = os.path.join(self.conf['tmp_dir'],repo)
+            subprocess.call(['git','clone','--mirror',rurl,rpath])
 
-        # Push each gh repo to Gitorious
-        #TODO
+            # Create repo if not present in Gitorious
+            if repo not in gs_repos:
+                self.gs_create_repo(repo, gh_repos[repo])
 
-        if self.temp:
-            pass#TODO: rm -rf self.conf['tmp_dir']
+            # Push GitHub repo to Gitorious
+            d = self.conf['gitorious']
+            d.update({'repo': repo})
+            gs_rurl = self.gs_push % d
+            # git remote add
+            subprocess.call(['git','--git-dir='+rpath,'remote','add','gitorious',gs_rurl])
+            # git push
+            subprocess.call(['git','--git-dir='+rpath,'push','--mirror','gitorious'])
+
+        if not self.temp:
+            shutil.rmtree(self.conf['tmp_dir'])
 
     def gh_list_repos(self):
-        url = self.gh_repos % {'user': self.conf['github']['user']}
+        url = self.gh_repos % self.conf['github']
         j = json.load(urllib2.urlopen(url))
-        repos = []
+        repos = {}
         for repo in j['repositories']:
-            repos.append(repo['name'])
+            repos[repo['name']] = repo['description']
         return repos
 
     def gs_list_repos(self):
-        pu = self.gs_project % {'proj':self.conf['gitorious']['project']}
+        pu = self.gs_project % self.conf['gitorious']
         p = urllib2.urlopen(pu)
         l = lxml.html.parse(p)
         repos = []
         for a in l.xpath('//a[starts-with(@href,"git://")]'):
             href = a.attrib['href'].strip()
-            exp = self.gs_regexp%{'proj':self.conf['gitorious']['project']}
+            exp = self.gs_regexp % self.conf['gitorious']
             m = re.match(exp, href)
             if m:
                 repos.append(m.group(1))
         return repos
 
-    def gs_create_repo(self, repo):
+    def gs_create_repo(self, repo, desc=''):
         if self.conf['gitorious']['password'] == None:
-            print "There is a new repository on GitHub named '%(repo)s', enter your password to create the repo on Gitorious" % {'repo':repo}
-            pass#TODO: collect password
-        pass#TODO: auth and create repo
+            print "One or more repositories on GitHub aren't present on Gitorious, please enter your password"
+            self.conf['gitorious']['password'] = getpass.getpass()
+
+        od = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+        urllib2.install_opener(od)
+
+        d = {}
+        for key in ['email','password']:
+            d[key] = self.conf['gitorious'][key]
+        data = urllib.urlencode(d)
+        # Login
+        #TODO: test what's returned
+        od.open(self.gs_login, data)
+
+        # Create repo
+        data = urllib.urlencode({'repository[name]': repo,
+            'repository[description]': desc})
+        od.open(self.gs_create % self.conf['gitorious'], data)
 
 if __name__ == '__main__':
     Copier().copy()
